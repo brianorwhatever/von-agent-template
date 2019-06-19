6e2abfce2fcf0ca7f477ca8e27cfe9d627c597f8
Brian Richter <brianorwhatever@gmail.com>
2019-06-19 11:23:12 -0700

    debugging openshift templates

---
+++ openshift/settings.sh
@@ -0,0 +1,8 @@
+export PROJECT_NAMESPACE="lclb-cllc"
+
+export GIT_REPO_URL="https://github.com/brianorwhatever/von-agent-template.git"
+export GIT_REF="master"
+
+export components="."
+export builds=""
+export images=""
